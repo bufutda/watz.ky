a24a3dd0eeb621965731981e9007f430a6dd926c
Bufutda <saw@tzky.ca>
2016-08-22 20:02:18 -0600

    Footer

--- main.js
+++ main.js
@@ -17,49 +17,52 @@
         return array;
     }
     var tiles = [];
-    var tileCounter = 60;
-    var night = true;
+    var tileCounter = 0;
+    var night = false;
     for (var i = 0; i < 60; i++) {
         tiles[i] = i;
     }
+    shuffle(tiles);
     setInterval(function () {
-        var aborted = false;
-        do {
-            if (tileCounter < 60) {
-                var tile = document.getElementById("innerTileWrapper").children[tiles[tileCounter]].classList;
-                tileCounter++;
-                if (night) {
-                    if (tile.contains("hovera")) {
-                        tile.toggle("hovera");
-                        aborted = false;
-                    } else if (tile.contains("hoverb")) {
-                        tile.toggle("hoverb");
-                        aborted = false;
-                    } else if (tile.contains("hoverc")) {
-                        tile.toggle("hoverc");
-                        aborted = false;
-                    } else if (tile.contains("hoverd")) {
-                        tile.toggle("hoverd");
-                        aborted = false;
-                    } else {
+        if (document.getElementsByTagName("bbsite")[0].scrollTop <= window.innerHeight) {
+            var aborted = false;
+            do {
+                if (tileCounter < 60) {
+                    var tile = document.getElementById("innerTileWrapper").children[tiles[tileCounter]].classList;
+                    tileCounter++;
+                    if (night) {
+                        if (tile.contains("hovera")) {
+                            tile.toggle("hovera");
+                            aborted = false;
+                        } else if (tile.contains("hoverb")) {
+                            tile.toggle("hoverb");
+                            aborted = false;
+                        } else if (tile.contains("hoverc")) {
+                            tile.toggle("hoverc");
+                            aborted = false;
+                        } else if (tile.contains("hoverd")) {
+                            tile.toggle("hoverd");
+                            aborted = false;
+                        } else {
+                            aborted = true;
+                        }
+                    } else if (tile.contains("hovera") || tile.contains("hoverb") || tile.contains("hoverc") || tile.contains("hoverd")) {
                         aborted = true;
+                    } else {
+                        var flipClass = decodeURIComponent("hover%" + (61 + Math.floor(Math.random() * 4)));
+                        tile.toggle(flipClass);
+                        aborted = false;
                     }
-                } else if (tile.contains("hovera") || tile.contains("hoverb") || tile.contains("hoverc") || tile.contains("hoverd")) {
-                    aborted = true;
                 } else {
-                    var flipClass = decodeURIComponent("hover%" + (61 + Math.floor(Math.random() * 4)));
-                    tile.toggle(flipClass);
+                    tileCounter = 0;
+                    shuffle(tiles);
+                    night = !night;
                     aborted = false;
                 }
-            } else {
-                tileCounter = 0;
-                shuffle(tiles);
-                night = !night;
-                aborted = false;
-            }
-        } while (aborted);
+            } while (aborted);
+        }
     }, 2000);
-    setTimeout(function () {
+    (function () {
         var elems = document.getElementsByClassName("fwrap");
         for (var i = 0; i < elems.length; i++) {
             elems[i].addEventListener("mouseenter", function (e) {
@@ -79,7 +82,7 @@
                 }
             });
         }
-    }, 100);
+    })();
     function resizeListener () { // eslint-disable-line require-jsdoc
         if (window.innerHeight / window.innerWidth < 0.6) {
             document.getElementById("outerTileWrapper").style.width = (Math.ceil(window.innerWidth / 100) * 100) + "px";
@@ -92,13 +95,13 @@
 })();
 
 /**
- * Scroll the document to the div#summary
+ * Scroll the document to the div#about
  * @returns {undefined}
  */
 function scrollToResume () {
     "use strict";
     var elem = document.getElementsByTagName("bbsite")[0];
-    var pos = document.getElementById("summary").offsetTop;
+    var pos = document.getElementById("about").offsetTop;
     var y = elem.scrollTop;
     y += Math.round((pos - y) * 0.3);
     if (Math.abs(y - pos) <= 2) {
